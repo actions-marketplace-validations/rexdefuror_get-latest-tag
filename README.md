043979e45128941a4a000c303fe71540a1211b4f
rexdefuror <srdjan.debic@gmail.com>
2022-12-01 13:21:45 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# get-latest-tag
-This is a simple GitHub action that finds a latest tag.
+[![Build](https://github.com/rexdefuror/get-latest-tag/actions/workflows/pipeline.yml/badge.svg)](https://github.com/rexdefuror/get-latest-tag/actions/workflows/pipeline.yml/badge.svg)
+
+## Description
+
+This is a simple Github action gets the latest tag. It sets an environment variable `LATEST_TAG` with the latest tag.
+
---
+++ src/index.ts
@@ -0,0 +1,83 @@
+import { getInput, exportVariable, setFailed } from '@actions/core';
+import { getOctokit } from '@actions/github';
+
+const main = async () => {
+    const token = getInput('token');
+    const repo = getInput('repo');
+    const owner = getInput('owner');
+    const prefix = getInput('prefix') || "";
+    const suffix = getInput('suffix') || "";
+    const regexInput = getInput('regex') || null;
+    const releasesOnly = (getInput('releasesOnly') === 'true');
+
+    const octokit = getOctokit(token);
+
+    if (releasesOnly) {
+        const releases = await octokit.rest.repos.listReleases({
+            owner: owner,
+            repo: repo
+        });
+
+        let foundTags = [];
+        for (const release of releases.data) {
+            const tagName = release.tag_name;
+
+            if (!tagName.startsWith(prefix)) {
+                continue;
+            }
+
+            if (regexInput && new RegExp(regexInput).test(tagName)) {
+                continue;
+            }
+
+            if (!tagName.endsWith(suffix)) {
+                continue;
+            }
+
+            foundTags.push(tagName);
+        }
+
+        if (foundTags.length == 0) {
+            setFailed("No tags found");
+            return;
+        }
+
+        exportVariable('LATEST_TAG', foundTags[0]);
+
+        return;
+    }
+
+    const tags = await octokit.rest.repos.listTags({
+        owner: owner,
+        repo: repo
+    });
+
+
+    let foundTags = [];
+    for (const tag of tags.data) {
+        const tagName = tag.name;
+
+        if (!tagName.startsWith(prefix)) {
+            continue;
+        }
+
+        if (regexInput && new RegExp(regexInput).test(tagName)) {
+            continue;
+        }
+
+        if (!tagName.endsWith(suffix)) {
+            continue;
+        }
+
+        foundTags.push(tagName);
+    }
+
+    if (foundTags.length == 0) {
+        setFailed("No tags found");
+        return;
+    }
+
+    exportVariable('LATEST_TAG', foundTags[0]);
+};
+
+main();
